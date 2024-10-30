a55e1a02b9870df18fd943b6a12548e2c6414311
shofiq18 <shafiqulislam25021998@gmail.com>
2024-10-30 13:08:00 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import DaisyNav from './components/DaisyNav'
+import NavBar from './components/NavBar'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <NavBar></NavBar>
+      <DaisyNav></DaisyNav>
+      <h1 className='bg-red-600 font-bold text-6xl'>Vite + React</h1>
+      
+ 
+    </>
+  )
+}
+
+export default App
