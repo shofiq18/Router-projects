68139a44bb6c788fe68cb4459024e6efc7778c58
shofiq18 <shafiqulislam25021998@gmail.com>
2024-10-31 20:16:57 +0600

    added navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import DaisyNav from './components/DaisyNav'
+
 import NavBar from './components/NavBar'
 import './App.css'
+
 
 function App() {
   
@@ -9,6 +11,7 @@ function App() {
     <>
       <NavBar></NavBar>
       <DaisyNav></DaisyNav>
+      
       <h1 className='bg-red-600 font-bold text-6xl'>Vite + React</h1>
       
  
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,25 @@
 
-
+import Link from "./link/Link";
 const NavBar = () => {
+    const routes = [
+        { id: 1, path: "/", name: "Home" },
+        { id: 2, path: "/about", name: "About" },
+        { id: 3, path: "/services", name: "Services" },
+        { id: 4, path: "/contact", name: "Contact" },
+        { id: 5, path: "/blog", name: "Blog" }
+      ];
+      
+    
+      
     return (
-        <div>
-            
-        </div>
+
+        <nav>
+           <ul className="md:flex ">
+           {
+                routes.map(route => <Link key={route.id} route={route}></Link> )
+            }
+           </ul>
+        </nav>
     );
 };
 
